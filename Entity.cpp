3d66904c46c454815ddcb7e925a5b6064b0a222e
zcerawesome <rishi.doddi@gmail.com>
2025-03-26 23:01:41 -0400

    Fixed perspective, fixing vertex clipp

---
+++ Entity.cpp
@@ -0,0 +1,31 @@
+#include "Entity.h"
+
+Entity::Entity(float x, float y, float z): x(x), y(y), z(z)
+{}
+
+Entity::Entity(float x, float y, float z, float x_degree, float y_degree, float z_degree):x(x), y(y), z(z), x_degree(x_degree), y_degree(y_degree), z_degree(z_degree)
+{}
+
+void Entity::update()
+{
+    x += x_vel;
+    y += y_vel;
+    z += z_vel;
+    x_vel = 0;
+    y_vel = 0;
+    z_vel = 0;
+}
+
+float& Entity::operator[](int index)
+{
+    switch(index)
+    {
+        case 0:
+            return x;
+        case 1:
+            return y;
+        case 2:
+            return z;
+    }
+    return x;
+}
